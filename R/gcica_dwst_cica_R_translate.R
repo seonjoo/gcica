14b2ff76abe66b5d2d29dbe16ad7e0de33485eaa
Qimin Zhang <qz2392@cumc.columbia.edu>
2020-07-31 18:08:24 -0400

    Add Prewhite part

--- R/gcica_dwst_cica_R_translate.R
+++ R/gcica_dwst_cica_R_translate.R
@@ -14,7 +14,7 @@
 
 gcica_bss_dwst = function (Xc, M = nrow(Xc[[1]][[1]]), W1 = diag(M),
                            tol = 1e-04, maxit = 20, nmaxit = 1,
-                           maxnmodels = 100, num_cores = 2) {
+                           maxnmodels = 100, prewhite = 1, num_cores = 2) {
   #################
   # Load packages #
   #################
