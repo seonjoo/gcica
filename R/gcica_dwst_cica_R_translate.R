c37f226bca8ce0c32de576b154930d5081d16585
Qimin Zhang <qz2392@cumc.columbia.edu>
2020-08-30 17:24:33 -0400

    Prewhite == T Delete random matrices examples

--- R/gcica_dwst_cica_R_translate.R
+++ R/gcica_dwst_cica_R_translate.R
@@ -14,7 +14,7 @@
 
 gcica_bss_dwst = function(Xc, M = nrow(Xc[[1]][[1]]), W1 = diag(M),
                            tol = 1e-04, maxit = 50, nmaxit = 1,
-                           maxnmodels = 10, prewhite = 1, num_cores = 2) {
+                           maxnmodels = 10, prewhite = T, num_cores = 2) {
   #################
   # Load packages #
   #################
@@ -87,7 +87,7 @@ gcica_bss_dwst = function(Xc, M = nrow(Xc[[1]][[1]]), W1 = diag(M),
   result = foreach(i = 1:num_group) %dopar% {
     #lapply(1:num_group, function(i){
     # Prewhite
-    if (prewhite == 1){
+    if (prewhite){
     svdcovmat = Xc[[i]]
     K = Xc[[i]]
     for (j in 1:num_group_subject[[i]]) {
